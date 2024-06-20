4f7b5b233c8a7c4d0a98e32e7085b04f0eb48a4a
AStefankiv <stefankif35@gmail.com>
2024-06-19 22:39:54 -0600

    Add days list

--- first_jan_sun?.js
+++ first_jan_sun?.js
@@ -1,9 +1,12 @@
 const date = new Date();
 const year = date.getFullYear();
+const day = date.getDay();
+const daysList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 
 const firstJanSun = (yearStart, yearEnd) => {
   for (let i = yearStart; i <= yearEnd; i++) {
-    const date = new Date(i, 0, 1);
+    const date = new Date(i, 0, 1);//i is the year, 0 is the month, 1 is the day
     if (date.getDay() === 0) {
       console.log(i);
     }

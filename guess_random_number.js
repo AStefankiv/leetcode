b085bc07ceed37a15deda97f2a7449ebe1c5eef2
AStefankiv <stefankif35@gmail.com>
2024-06-20 22:24:28 -0600

    guess_random_number.js refactored

--- guess_random_number.js
+++ guess_random_number.js
@@ -1,12 +1,11 @@
-let randomNum = Math.floor(Math.random() * 10);
-
 const userInput = process.argv[2];
 
 const guessRamdomNumber = (userInput) => {
+  let randomNum = Math.floor(Math.random() * 10);
   if (userInput == randomNum) {
     console.log('You guessed the right number!');
   } else {
-    console.log(`You guessed the wrong number. The right number is ${randomNum}`);
+    console.log(`You guessed the wrong number! The correct number is ${randomNum}`);
   }
 }
 

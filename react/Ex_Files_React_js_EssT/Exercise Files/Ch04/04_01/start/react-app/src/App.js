fbe69966a599e1112b9f025c2723f7c64e0e0ef4
AStefankiv <stefankif35@gmail.com>
2024-06-30 17:23:57 -0600

    Create simple routes

--- react/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_01/start/react-app/src/App.js
+++ react/Ex_Files_React_js_EssT/Exercise Files/Ch04/04_01/start/react-app/src/App.js
@@ -1,34 +1,32 @@
 import './App.css';
 
-const tahoe_peaks = [
-  {name: "Freel Peak", elevation: 10891},
-  {name: "Monument Peak", elevation: 10067},
-  {name: "Pyramid Peak", elevation: 9983},
-  {name: "Mt. Tallac", elevation: 9735}
-];
-
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>
-  )
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
-function App() {
+export function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+
+export function App() {
     return (
-      <List data={tahoe_peaks} renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-        {item.name} - {item.elevation} ft.
-        </>
-      )} />
+      <Home />
     );
 }
 

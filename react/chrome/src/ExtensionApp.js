79d88cfedf4f76df4a972704c6b7753fb9d6f5f2
AStefankiv <stefankif35@gmail.com>
2024-07-06 18:13:06 -0600

    Create counter extension & modify css

--- react/chrome/src/ExtensionApp.js
+++ react/chrome/src/ExtensionApp.js
@@ -12,8 +12,10 @@ function ExtensionApp() {
       <header className="ExtensionApp-header">
         <h1>Chrome extension</h1>
         <p>Count: {count}</p>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+        <div className="button-container">
+        <button onClick={increment}>Increment +</button>
+        <button onClick={decrement}>Decrement -</button>
+        </div>
       </header>
     </div>
   );

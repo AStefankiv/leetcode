fd0ae7daa18704a90bb17ac53f48ee9cc273a921
AStefankiv <stefankif35@gmail.com>
2024-07-24 22:10:38 -0600

    23

---
+++ 23_create_new_string.js
@@ -0,0 +1,11 @@
+const newString = (str) => {
+  let firstChar = str.charAt(0);
+  let lastChar = str.charAt(str.length - 1);
+  if (str.length < 2) {
+    return str;
+  } else {
+    return lastChar + str.slice(1, -1) + firstChar;
+  }
+}
+
+console.log(newString('Hello')); // Output: oellH

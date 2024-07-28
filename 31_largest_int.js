2058325bf98d8a263e5c694080c5b51e4fb32b9f
AStefankiv <stefankif35@gmail.com>
2024-07-28 09:45:30 -0600

    31 use arrays

--- 31_largest_int.js
+++ 31_largest_int.js
@@ -1,4 +1,19 @@
-const largestInt = (a, b, c) => {
-  return Math.max(a, b, c);
+const largestArraySum = (a, b, c) => {
+  const sumA = a.reduce((acc, curr) => acc + curr, 0);
+  const sumB = b.reduce((acc, curr) => acc + curr, 0);
+  const sumC = c.reduce((acc, curr) => acc + curr, 0);
+  const largestSum = Math.max(sumA, sumB, sumC);
+  if (largestSum === sumA) {
+    return a;
+  }
+  if (largestSum === sumB) {
+    return b;
+  }
+  return c;
 }
-console.log(largestInt(1, 2, 3)); // 3
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const array3 = [11, 12, 13, 14, 15];
+
+console.log(largestArraySum(array1, array2, array3)); // [11, 12, 13, 14, 15]

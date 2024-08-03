088ddbb9ea148b2bd1fae4e66bb43ecc534c9800
AStefankiv <stefankif35@gmail.com>
2024-08-02 22:17:29 -0600

    43

--- 43_right_most_digit.js
+++ 43_right_most_digit.js
@@ -1,4 +1,18 @@
 const rightMostDigit = (num1, num2, num3) => {
-  return (num1 % 10) * (num2 % 10) * (num3 % 10);
+  const num1Split = num1.toString().split('');
+  const num2Split = num2.toString().split('');
+  const num3Split = num3.toString().split('');
+
+  const num1LastDigit = num1Split.pop();
+  const num2LastDigit = num2Split.pop();
+  const num3LastDigit = num3Split.pop();
+
+  if (num1LastDigit === num2LastDigit && num2LastDigit === num3LastDigit) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log(rightMostDigit(123, 456, 789)); // false
+console.log(rightMostDigit(22, 32, 42)); // true

b8b90ca36e9e0f26b28b90105667218297db285c
AStefankiv <stefankif35@gmail.com>
2024-08-03 22:28:53 -0600

    Example 2

--- 43_right_most_digit.js
+++ 43_right_most_digit.js
@@ -5,3 +5,4 @@ const rightMostDigit = (num1, num2, num3) => {
 console.log(rightMostDigit(23, 33, 43)); // true
 console.log(rightMostDigit(23, 33, 44)); // false
 console.log(rightMostDigit(23, 33, 45)); // false
+console.log(rightMostDigit(23, 33, 46)); // false

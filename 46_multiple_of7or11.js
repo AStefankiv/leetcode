af5b31f53faa6b8da86118e1372fe14115345b7a
AStefankiv <stefankif35@gmail.com>
2024-08-05 21:32:03 -0600

    48

--- 46_multiple_of7or11.js
+++ 46_multiple_of7or11.js
@@ -1,12 +1,14 @@
-const multipleOf7Or11 = (num1, num2) => {
-  if (num1 > 0 && num2 > 0) {
-    if (num1 % 7 === 0 || num1 % 11 === 0 || num2 % 7 === 0 || num2 % 11 === 0) {
-      return true;
-    }
-    return false;
-  }
-}
+// const multipleOf7Or11 = (num1, num2) => {
+//   if (num1 > 0 && num2 > 0) {
+//     if (num1 % 7 === 0 || num1 % 11 === 0 || num2 % 7 === 0 || num2 % 11 === 0) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
 
-console.log(multipleOf7Or11(14, 21)) // true
-console.log(multipleOf7Or11(14, 22)) // true
-console.log(multipleOf7Or11(13, 23)) // false
+// console.log(multipleOf7Or11(14, 21)) // true
+// console.log(multipleOf7Or11(14, 22)) // true
+// console.log(multipleOf7Or11(13, 23)) // false
+
+var myArray = 
---
+++ 48_reverse_string.js
@@ -0,0 +1,7 @@
+const reverse = (str) => {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverse('hello')); // olleh
+console.log(reverse('world')); // dlrow
+console.log(reverse('javascript')); // tpircsavaj

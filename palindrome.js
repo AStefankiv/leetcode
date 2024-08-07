ed89f3a470b76f865f3ed7bbf44317e1a9f75207
AStefankiv <stefankif35@gmail.com>
2024-08-06 18:59:10 -0600

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+const palindrome = (string) => {
+  let opposite = string.split('').reverse(' ').join('')
+  if (opposite === string) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindrome('hello'));
+console.log(palindrome('level'));

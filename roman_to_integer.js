bdefd8925ed68772487dde6ec8e940d761ca0ee6
AStefankiv <stefankif35@gmail.com>
2024-08-08 22:03:12 -0600

    roman to integer

--- roman_to_integer.js
+++ roman_to_integer.js
@@ -3,4 +3,14 @@ const romanToInteger = (s) => {
   let result = 0;
   for (let i = 0; i < s.length; i++) {
     if (roman[s[i]] < roman[s[i + 1]]) {
-      result += roman[s[i + 1]] -
+      result += roman[s[i + 1]] - roman[s[i]];
+      i++;
+    }
+    else {
+      result += roman[s[i]];
+    }
+  }
+  return result;
+}
+
+console.log(romanToInteger('III')); // 3

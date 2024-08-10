60ed507e8a7d27cb4de00f4d6f999a7ef9a27f13
AStefankiv <stefankif35@gmail.com>
2024-08-09 18:14:24 -0600

    longest common prefix

---
+++ longest_common_prefix.js
@@ -0,0 +1,12 @@
+const longestCommonPrefix = (array) => {
+  if (array.length === 0) return '';
+  let prefix = array[0];// flower
+  for (let i = 1; i < array.length; i++) {
+    while (array[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']));

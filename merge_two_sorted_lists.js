2c93e6c3bd68aff96da9b69a410f157ca50e4aab
AStefankiv <stefankif35@gmail.com>
2024-08-10 16:26:53 -0600

    Merge 2 arrays + add examples

--- merge_two_sorted_lists.js
+++ merge_two_sorted_lists.js
@@ -2,3 +2,5 @@ const two_arrays = (arr1, arr2) => {
   const merge = arr1.concat(arr2);
   return merge.sort((a, b) => a - b);
 };
+
+console.log(two_arrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]

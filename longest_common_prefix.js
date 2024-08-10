d08b18d465abb9a3abb48ae9cbf7ccd7c36e01ea
AStefankiv <stefankif35@gmail.com>
2024-08-09 18:15:10 -0600

    longest common prefix

--- longest_common_prefix.js
+++ longest_common_prefix.js
@@ -2,7 +2,7 @@ const longestCommonPrefix = (array) => {
   if (array.length === 0) return '';
   let prefix = array[0];// flower
   for (let i = 1; i < array.length; i++) {
-    while (array[i].indexOf(prefix) !== 0) {
+    while (array[i].indexOf(prefix) !== 0) {//while the prefix is not at the beginning of the array[i]
       prefix = prefix.substring(0, prefix.length - 1);
     }
   }
@@ -10,3 +10,5 @@ const longestCommonPrefix = (array) => {
 }
 
 console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
+console.log(longestCommonPrefix(['dog', 'racecar', 'car']));
+console.log(longestCommonPrefix(['car', 'car', 'cactus']));

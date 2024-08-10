2fed66ed19ceeebe7a6d76427c969174a8b4496d
AStefankiv <stefankif35@gmail.com>
2024-08-09 18:23:36 -0600

    valid parenthesis

---
+++ valid_parenthesis.js
@@ -0,0 +1,15 @@
+const parenthesis = (str) => {
+  const open = ['(', '[', '{'];
+  const close = [')', ']', '}'];
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {//if open is found in the string
+      if (!close.includes(str[i + 1]))
+        return false;
+    }
+  }
+  return true;
+}
+
+console.log(parenthesis('()')); // true
+console.log(parenthesis('()[]{}')); // true
+console.log(parenthesis('(]')); // false

26fa0b04fad2360341224da9d12b0dc0c898fcd5
AStefankiv <stefankif35@gmail.com>
2024-08-11 20:35:57 -0600

    remove duplicates with new Set

--- remove_duplicates_array.js
+++ remove_duplicates_array.js
@@ -1,12 +1,13 @@
 const removeDuplicates = (arr) => {
   // return arr.filter((item, index) => arr.indexOf(item) === index);
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!result.includes(arr[i])) {
-      result.push(arr[i]);
-    }
-  }
-  return result;
+  return [...new Set(arr)];
+  // const result = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (!result.includes(arr[i])) {
+  //     result.push(arr[i]);
+  //   }
+  // }
+  // return result;
 }
   
 const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];

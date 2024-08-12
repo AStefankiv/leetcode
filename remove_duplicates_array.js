68cbf7df64fbb0b50195e34f84dfe98c8d7e0af2
AStefankiv <stefankif35@gmail.com>
2024-08-11 18:30:57 -0600

    remove duplicates

--- remove_duplicates_array.js
+++ remove_duplicates_array.js
@@ -1,5 +1,13 @@
 const removeDuplicates = (arr) => {
-  return arr.filter((item, index) => arr.indexOf(item) === index);
+  // return arr.filter((item, index) => arr.indexOf(item) === index);
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
+  
 const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
 console.log(removeDuplicates(arr)); // [1, 2, 3, 4, 5, 6, 7]

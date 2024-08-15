dc79f18fa0509ad48ac2c48709d2e75927f664e9
AStefankiv <stefankif35@gmail.com>
2024-08-14 18:53:47 -0600

    Review two sum

--- two_sum.js
+++ two_sum.js
@@ -1,13 +1,13 @@
-const twoSum = (nums, target) => {
+const two_sum = (nums, target) => {
   for (let i = 0; i < nums.length; i += 1) {
     for (let j = i + 1; j < nums.length; j += 1) {
       if (nums[i] + nums[j] === target) {
-        return [i, j]
+        return [nums[i], nums[j]];
       }
     }
   }
 }
 
-const arrayNums = [2, 4, 6, 8, 10];
-const targetNum = 12
-console.log(twoSum(arrayNums, targetNum))
+const nums = [33, 44, 55, 66, 77, 88, 99];
+const target = 143;
+console.log(two_sum(nums, target)); // [44, 99]

778a9bece4385c43f2c9303ca80f825bd8e5976b
AStefankiv <stefankif35@gmail.com>
2024-08-18 21:48:36 -0600

    removeDuplicates - done

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,3 +1,12 @@
 const removeDuplicates = (arr) => {
   const result = [];
-  for 
+  for (let i = 0; i < arr.length; i += 1) {
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+console.log(removeDuplicates(arr)); // [1, 2, 3, 4, 5, 6, 7]

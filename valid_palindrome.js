3b27d2068b7bf108cff79878f0c05d607a111332
AStefankiv <stefankif35@gmail.com>
2024-08-25 09:26:56 -0600

    valid palindrome -done

--- valid_palindrome.js
+++ valid_palindrome.js
@@ -1,5 +1,11 @@
 const sentence1 = "A man, a plan, a canal: Panama";
-const sentence1ToLower = sentence1.toLowerCase();
-const sentence1WithoutSpecialChars = sentence1ToLower.replace(/[^a-zA-Z0-9]/g, '');
-const sentence1Reversed = sentence1WithoutSpecialChars.split('').reverse().join('');
-console.log(sentence1WithoutSpecialChars === sentence1Reversed); // true
+
+const validPalindrome = (s) => {
+  const sToLower = s.toLowerCase();
+  const sWithoutSpecialChars = sToLower.replace(/[^a-zA-Z0-9]/g, '');
+  const sReversed = sWithoutSpecialChars.split('').reverse().join('');
+  return sWithoutSpecialChars === sReversed;
+}
+
+console.log(validPalindrome(sentence1)); // true
+console.log(validPalindrome("race a car")); // false

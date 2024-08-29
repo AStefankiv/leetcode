600a65eb72fc1f74a4ea6398e3cf5c93dd6fac71
AStefankiv <stefankif35@gmail.com>
2024-08-29 17:56:59 -0600

    remove extra element - complete

--- remove_extra_element.js
+++ remove_extra_element.js
@@ -1,8 +1,19 @@
 const array = [1, 2, 3, 9, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; i < array.length; i++) {
-  if (array[i] + 1 !== array[i + 1]) {
-    console.log(array[i + 1]);
-    break;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] + 1 !== array[i + 1]) {
+//     console.log(array[i + 1]);
+//     break;
+//   }
+// }
+
+const extraElement = (array) => {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] + 1 !== array[i + 1]) {
+      return 'Extra element: ' + array[i + 1] + ' at index: ' + (i + 1);
+    }
   }
+  return 'No extra element found';
 }
+
+console.log(extraElement(array)); // 9

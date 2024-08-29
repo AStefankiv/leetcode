3c846e924a867b467dcac7dac7d73de14da69578
AStefankiv <stefankif35@gmail.com>
2024-08-28 19:35:23 -0600

    valid phone numbers - remove comments

--- valid_phone_numbers.js
+++ valid_phone_numbers.js
@@ -2,15 +2,6 @@ const fs = require('fs');
 
 const phoneNums = './phone_nums.txt'
 
-// fs.readFile(phoneNums, 'utf8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   } else {
-//     console.log(data);
-//   }
-// });
-
 const readPhoneNums = (file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {

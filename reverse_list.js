59aa02b631ca564392abd6fd8c21acec5b89db84
AStefankiv <stefankif35@gmail.com>
2024-08-30 19:34:54 -0600

    reverse list - done

--- reverse_list.js
+++ reverse_list.js
@@ -1,3 +1,7 @@
 const listNum = [1, 2, 3, 4, 5];
-const reverse = listNum.reverse();
-console.log(reverse); // [ 5, 4, 3, 2, 1 ]
+
+const reverseArray = (array) => {
+  return array.reverse();
+}
+
+console.log(reverseArray(listNum)); // [ 5, 4, 3, 2, 1 ]

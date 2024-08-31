d109145523a34e7443faf100e017568cf2f4a0ad
AStefankiv <stefankif35@gmail.com>
2024-08-31 15:48:40 -0600

    React learn states3

--- react/chrome/src/Name1.js
+++ react/chrome/src/Name1.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
 const Name1 = () => {
-  const [firstName, setFirstName] = useState('Taylor');
-  const [lastName, setLastName] = useState('Swift');
+  // const [firstName, setFirstName] = useState('Taylor');
+  // const [lastName, setLastName] = useState('Swift');
 
-  const fullName = firstName + ' ' + lastName;
+  // const fullName = firstName + ' ' + lastName;
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <label>First Name:</label>
+  //       <input
+  //         type="text"
+  //         value={firstName}
+  //         onChange={(e) => setFirstName(e.target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label>Last Name:</label>
+  //       <input
+  //         type="text"
+  //         value={lastName}
+  //         onChange={(e) => setLastName(e.target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       <label>Full Name:</label>
+  //       <input type="text" value={fullName} readOnly />
+  //     </div>
+  //   </div>
+  // );
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('Timer is running...');
+    }, 1000);
+
+    // Cleanup function to clear the timer when the component unmounts
+    return () => clearInterval(timer);
+  }, []); // Empty array means the timer is set up once on mount
 
   return (
     <div>
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Full Name:</label>
-        <input type="text" value={fullName} readOnly />
-      </div>
+      Check your console for timer logs!
     </div>
   );
 }

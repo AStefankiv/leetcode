e1c06b4d74dc56a385d5788e44a3e7a041d24ea9
AStefankiv <stefankif35@gmail.com>
2024-09-02 21:53:09 -0600

    summary ranges - function

--- summary_ranges.js
+++ summary_ranges.js
@@ -11,3 +11,20 @@ for (let i = 0; i < nums.length; i += 1) {
     console.log(start);
   }
 }
+
+const summaryRanges = (nums) => {
+  const result = [];
+  for (let i = 0; i < nums.length; i += 1) {
+    let start = nums[i];
+    while (nums[i + 1] - nums[i] === 1) {
+      i += 1;
+    }
+    if (start !== nums[i]) {
+      result.push(start + '->' + nums[i]);
+    } else {
+      result.push(start.toString());
+    }
+  }
+  return result;
+}
+

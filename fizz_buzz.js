8cda1439300b47396693237059ba9e29d08528e0
AStefankiv <stefankif35@gmail.com>
2024-09-16 22:02:39 -0600

    Add examples

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -13,3 +13,7 @@ const fizzBuzz = (n) => {
     }
     return result;
 }
+
+console.log(fizzBuzz(3)); // ['1', '2', 'Fizz']
+console.log(fizzBuzz(5)); // ['1', '2', 'Fizz', '4', 'Buzz']
+console.log(fizzBuzz(15)); // ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']

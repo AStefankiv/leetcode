f63df87420ba933722ffc9cf47e2be1e4d36c993
AStefankiv <stefankif35@gmail.com>
2024-09-16 22:02:20 -0600

    create function

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,9 +1,15 @@
 const fizzBuzz = (n) => {
     let result = [];
-    if (n % 3 === 0 && n % 5 === 0) {
-        result.push('FizzBuzz');
+    for (let i = 0; i <= n; i += 1) {
+        if (i % 15 === 0) {
+            result.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+            result.push('Fizz');
+        } else if (i % 5 === 0) {
+            result.push('Buzz');
+        } else {
+            result.push(i.toString());
+        }
     }
     return result;
-};
-
-console.log(fizzBuzz(15)); // ['FizzBuzz']
+}

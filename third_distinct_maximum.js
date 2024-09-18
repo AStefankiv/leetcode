6552c29741202c48f442736e3453337faecab157
AStefankiv <stefankif35@gmail.com>
2024-09-17 21:25:29 -0600

    third maximum number - function

--- third_distinct_maximum.js
+++ third_distinct_maximum.js
@@ -1,7 +1,11 @@
-const numbers = [3, 2, 1];
-numbers.sort((a, b) => b - a);
-console.log(numbers);
+const thirdMax = (nums) => {
+    nums.sort((a, b) => b - a);
+    let distinct = [...new Set(nums)];
+    if (distinct.length < 3) {
+        return distinct[0];
+    }
+    return distinct[2];
+}
 
-// for (let i = 0; i < numbers.length; i += 1) {
-//     console.log(numbers[i]);
-// }
+console.log(thirdMax([3, 2, 1])); // 1
+console.log(thirdMax([1, 2])); // 2

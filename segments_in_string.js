86b01d3b8ee76416f938ab1ee4bde0adc3c8a5a7
AStefankiv <stefankif35@gmail.com>
2024-09-18 21:25:39 -0600

    segments in a string

---
+++ segments_in_string.js
@@ -0,0 +1,12 @@
+// const string = "Hello, my name is John";
+
+// const segments = string.split(" ");
+// console.log(segments); // ["Hello,", "my", "name", "is", "John"]
+// console.log(segments.length); // 5
+
+// const numOfSegments = (string) => {
+//     return string.split(" ").length;
+// }
+
+console.log(numOfSegments("Hello, my name is John")); // 5
+console.log(numOfSegments("This is a test")); // 4

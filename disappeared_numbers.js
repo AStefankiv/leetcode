cd54b86c6bdf1717ab4696db7591cb120f0ce6da
AStefankiv <stefankif35@gmail.com>
2024-09-19 21:47:38 -0600

    Disappeared nums - function

--- disappeared_numbers.js
+++ disappeared_numbers.js
@@ -1,12 +1,26 @@
 const nums = [4,3,2,7,8,2,3,1];
-const sortedNums = nums.sort((a, b) => a - b);
-const uniqueNums = [...new Set(sortedNums)];
-console.log(uniqueNums); // [1, 2, 3, 4, 7, 8]
-const disappearedNums = [];
-for (let i = 1; i <= uniqueNums.length; i++) {
-    if (!uniqueNums.includes(i)) {
-        disappearedNums.push(i);
+// const sortedNums = nums.sort((a, b) => a - b);
+// const uniqueNums = [...new Set(sortedNums)];
+// console.log(uniqueNums); // [1, 2, 3, 4, 7, 8]
+// const disappearedNums = [];
+// for (let i = 1; i <= uniqueNums.length; i++) {
+//     if (!uniqueNums.includes(i)) {
+//         disappearedNums.push(i);
+//     }
+// }
+
+// console.log(disappearedNums); // [5, 6]
+
+const findDisappearedNumbers = (nums) => {
+    const sortedNums = nums.sort((a, b) => a - b);
+    const uniqueNums = [...new Set(sortedNums)];
+    const disappearedNums = [];
+    for (let i = 1; i <= uniqueNums.length; i++) {
+        if (!uniqueNums.includes(i)) {
+            disappearedNums.push(i);
+        }
     }
+    return disappearedNums;
 }
 
-console.log(disappearedNums); // [5, 6]
+console.log(findDisappearedNumbers(nums)); // [5, 6]

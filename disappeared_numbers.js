6320970b0437ec545cbb99a39db424a95f6d4afe
AStefankiv <stefankif35@gmail.com>
2024-09-19 21:47:06 -0600

    Disappeared nums - logic

--- disappeared_numbers.js
+++ disappeared_numbers.js
@@ -1,3 +1,12 @@
 const nums = [4,3,2,7,8,2,3,1];
 const sortedNums = nums.sort((a, b) => a - b);
-console.log(sortedNums); // [1, 2, 2, 3, 3, 4, 7, 8]
+const uniqueNums = [...new Set(sortedNums)];
+console.log(uniqueNums); // [1, 2, 3, 4, 7, 8]
+const disappearedNums = [];
+for (let i = 1; i <= uniqueNums.length; i++) {
+    if (!uniqueNums.includes(i)) {
+        disappearedNums.push(i);
+    }
+}
+
+console.log(disappearedNums); // [5, 6]

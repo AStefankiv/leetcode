6962827fdc92b2a97c146079ace65f18da00f1ee
AStefankiv <stefankif35@gmail.com>
2024-09-28 22:35:10 -0600

    license key formatting - remove comments

--- license_key_formatting.js
+++ license_key_formatting.js
@@ -1,12 +1,3 @@
-// const licenseKey = "5F3Z-2e-9-w"
-// const characters = 4
-
-// const alphanumericString = licenseKey.replace(/[^a-zA-Z0-9]/g, '');
-// console.log(alphanumericString);
-
-// const splitByFour = alphanumericString.match(new RegExp(`.{1,${characters}}`, 'g')).join('-').toUpperCase();
-// console.log(splitByFour);
-
 const licenseKeyFormatting = (licenseKey, characters) => {
   const alphanumericString = licenseKey.replace(/[^a-zA-Z0-9]/g, '');
   return alphanumericString.match(new RegExp(`.{1,${characters}}`, 'g')).join('-').toUpperCase();

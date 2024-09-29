c9c33d6d7097ef54dd175e20201dec08f8c6deed
AStefankiv <stefankif35@gmail.com>
2024-09-29 16:00:00 -0600

    max consecutive done

--- max_consecutive_ones.js
+++ max_consecutive_ones.js
@@ -1,14 +1,31 @@
 const nums = [1,1,0,1,1,1];
-let count = 0;
-let max = 0;
-for (let i = 0; i < nums.length; i += 1) {
-  if (nums[i] === 1) {
-    count += 1;
-    max = Math.max(max, count);
+// let count = 0;
+// let max = 0;
+// for (let i = 0; i < nums.length; i += 1) {
+//   if (nums[i] === 1) {
+//     count += 1;
+//     max = Math.max(max, count);
+//   }
+//   else {
+//     count = 0;
+//   }
+// }
+
+// console.log(max); // 3
+
+const findMaxConsecutiveOnes = (nums) => {
+  let count = 0;
+  let max = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] === 1) {
+      count += 1;
+      max = Math.max(max, count);
+    }
+    else {
+      count = 0;
+    }
   }
-  else {
-    count = 0;
-  }
+  return max;
 }
 
-console.log(max); // 3
+console.log(findMaxConsecutiveOnes(nums)); // 3

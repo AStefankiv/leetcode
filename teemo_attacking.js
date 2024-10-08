2629e6b6ad66538f2e7dab0edadbd2381c2ee418
AStefankiv <stefankif35@gmail.com>
2024-10-07 22:25:08 -0600

    Create function

--- teemo_attacking.js
+++ teemo_attacking.js
@@ -1,3 +1,15 @@
 const timeSeries = [1, 4];
 const duration = 2;
 
+const findPoisonedDuration = (timeSeries, duration) => {
+  if (timeSeries.length === 0) {
+    return 0;
+  }
+  let total = 0;
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+  }
+  return total + duration;
+}
+
+console.log(findPoisonedDuration(timeSeries, duration)); // 4

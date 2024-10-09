63f0e133b480bba11c767801dc9d5d88a26c9a95
AStefankiv <stefankif35@gmail.com>
2024-10-08 22:09:27 -0600

    Create function

--- keyboardRow.js
+++ keyboardRow.js
@@ -2,22 +2,29 @@ const firstRow = 'qwertyuiop';
 const secondRow = 'asdfghjkl';
 const thirdRow = 'zxcvbnm';
 
-// const words = ["Hello","Alaska","Dad","Peace"];
-const word = ["Dad"];
-const wordLowerCase = word[0].toLowerCase();
+const words = ["Hello","Alaska","Dad","Peace"];
 
 const findWords = (words) => {
-  let result;
-  for (let i = 0; i < words.length; i += 1) {
-    if (firstRow.includes(words[i])) {
-      result = "firstRow";
-    } else if (secondRow.includes(words[i])) {
-      result = "secondRow";
-    } else if (thirdRow.includes(words[i])) {
-      result = "thirdRow";
+  const result = [];
+  words = words.map(word => word.toLowerCase());
+  for (let i = 0; i < words.length; i+= 1) {
+    let firstRowCount = 0;
+    let secondRowCount = 0;
+    let thirdRowCount = 0;
+    for (let j = 0; j < words[i].length; j+= 1) {
+      if (firstRow.includes(words[i][j])) {
+        firstRowCount += 1;
+      } else if (secondRow.includes(words[i][j])) {
+        secondRowCount += 1;
+      } else if (thirdRow.includes(words[i][j])) {
+        thirdRowCount += 1;
+      }
+    }
+    if (firstRowCount === words[i].length || secondRowCount === words[i].length || thirdRowCount === words[i].length) {
+      result.push(words[i]);
     }
   }
   return result;
 }
 
-console.log(findWords(wordLowerCase));
+console.log(findWords(words));

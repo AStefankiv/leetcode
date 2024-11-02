1a6d188de3e9323e66f9286e8f80865a8fca49e3
AStefankiv <stefankif35@gmail.com>
2024-11-01 21:27:27 -0600

    add firebase

---
+++ firebase-tutorial/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFXsjGUN1FY0fMhiyN9RFDkJ1pJ5A4kQ0",
+  authDomain: "tutorial-3ccb5.firebaseapp.com",
+  projectId: "tutorial-3ccb5",
+  storageBucket: "tutorial-3ccb5.firebasestorage.app",
+  messagingSenderId: "407813217870",
+  appId: "1:407813217870:web:04fdb3fcbba924405cad1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);

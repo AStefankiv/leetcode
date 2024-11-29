c308ebb37aa2b56cb7af797c370dd846fc0dbd99
AStefankiv <stefankif35@gmail.com>
2024-11-28 22:13:10 -0700

    Segments 2

--- number_of_segments.js
+++ number_of_segments.js
@@ -1,7 +1,4 @@
 const sentence = "The quick brown fox jumps over the lazy dog.";
-let array = [];
-const words = sentence.split(" ");
-console.log(words.length);
 
 const countSegments = (str) => {
   const words = str.split(" ");

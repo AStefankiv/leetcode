438b60c1e43241233049241c9cd62724b102af80
AStefankiv <stefankif35@gmail.com>
2024-11-30 15:02:31 -0700

    cookies

--- cookies.js
+++ cookies.js
@@ -14,13 +14,15 @@ const findContentChildren = (g, s) => {
   g.sort((a, b) => a - b);
   s.sort((a, b) => a - b);
 
-  let count = 0;
+  let contentChildren = 0;
   for (let i = 0; i < s.length; i += 1) {
-    if (s[i] >= g[count]) {
-      count += 1;
+    if (s[i] >= g[contentChildren]) {
+      contentChildren += 1;
+
+      if (contentChildren === g.length) break;
     }
   }
-  return count;
+  return contentChildren;
 }
 
 console.log(findContentChildren([1, 2, 3], [1, 1]));

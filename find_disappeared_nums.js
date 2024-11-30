cea6f84ead0ab4a0ae1e0307a15004872eaa9508
AStefankiv <stefankif35@gmail.com>
2024-11-29 22:05:56 -0700

    Find disappeared numbers

---
+++ find_disappeared_nums.js
@@ -0,0 +1,17 @@
+const findDisappearedNumbers = (nums) => {
+  const sorted = new Set(nums)
+  let missedNums = [];
+
+  const start = Math.min(...sorted);
+  const end = Math.max(...sorted);
+
+  for (let i = start; i <= end; i += 1) {
+    if (!sorted.has(i)) {
+      missedNums.push(i);
+    }
+  }
+
+  return missedNums;
+}
+
+console.log(findDisappearedNumbers([56, 57, 59, 61, 62]));

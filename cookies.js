f36122e669eea6f5d1bc90f3c65a458c6cf1d47a
AStefankiv <stefankif35@gmail.com>
2024-11-30 15:00:47 -0700

    cookies

--- cookies.js
+++ cookies.js
@@ -1,11 +1,27 @@
-const arrayOne = [1, 2];
-const arrayTwo = [1, 2, 3];
+// const arrayOne = [1, 2];
+// const arrayTwo = [1, 2, 3];
 
-let count = 0;
-for (let i = 0; i < arrayTwo.length; i += 1) {
-  if (arrayTwo[i] >= arrayOne[i]) {
-    count += 1;
+// let count = 0;
+// for (let i = 0; i < arrayTwo.length; i += 1) {
+//   if (arrayTwo[i] >= arrayOne[i]) {
+//     count += 1;
+//   }
+// }
+
+// console.log(count);
+
+const findContentChildren = (g, s) => {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+
+  let count = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] >= g[count]) {
+      count += 1;
+    }
   }
+  return count;
 }
 
-console.log(count);
+console.log(findContentChildren([1, 2, 3], [1, 1]));
+console.log(findContentChildren([1, 2], [1, 2, 3]));

13f1bca6269bc02968a3060b9ecd6e3ae0633d54
AStefankiv <stefankif35@gmail.com>
2024-12-02 21:09:37 -0700

    License Key Formatting - 3

--- License_Key_Formatting.js
+++ License_Key_Formatting.js
@@ -1,14 +1,3 @@
-const code = "5F3Z-2e-9-wR";
-const codeShort = code.replace(/-/g, "");
-console.log(codeShort); // 5F3Z2e9w
-
-let result = [];
-for (let i = 0; i < codeShort.length; i += 4) {
-  result.push(codeShort.slice(i, i + 4));
-}
-
-console.log(result.join("-")); // 5F3Z-2e9w
-
 const divideCode = (code, division) => {
   const codeShort = code.replace(/-/g, "");
   let result = [];

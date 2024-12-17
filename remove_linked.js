3c5ba95d7d8c4e45efd72574ccbf0276063ff3d3
AStefankiv <stefankif35@gmail.com>
2024-12-16 22:16:40 -0700

    remove linked

--- remove_linked.js
+++ remove_linked.js
@@ -1,3 +1,14 @@
+// const array = [1,2,6,3,4,5,6];
+// const val = 6;
+// const newArray = array.filter((item) => item !== val);
+
+// console.log(newArray); // [1, 2, 3, 4, 5]
+
+const removeElement = (nums, val) => {
+  return nums.filter((item) => item !== val);
+}
+
 const array = [1,2,6,3,4,5,6];
 const val = 6;
-const newArray = array.filter((item) => item !== val);
+
+console.log(removeElement(array, val)); // [1, 2, 3, 4, 5]

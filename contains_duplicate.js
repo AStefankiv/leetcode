f1132cf0182f9479da6b93f26474aba1e8ff3fe7
AStefankiv <stefankif35@gmail.com>
2024-12-17 22:30:41 -0700

    contains duplicate

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -1,14 +1,31 @@
 const nums = [1,2,3,1];
-let hash = {};
-for (let i = 0; i < nums.length; i += 1) {
-  if (!hash[nums[i]]) {
-    hash[nums[i]] = 1;
-  } else {
-    hash[nums[i]] += 1;
+// let hash = {};
+// for (let i = 0; i < nums.length; i += 1) {
+//   if (!hash[nums[i]]) {
+//     hash[nums[i]] = 1;
+//   } else {
+//     hash[nums[i]] += 1;
+//   }
+//   if (hash[nums[i]] > 1) {
+//     console.log(true);
+//     break;
+//   }
+// }
+// console.log(hash);
+
+const containsDuplicate = (nums) => {
+  let hash = {};
+  for (let i = 0; i < nums.length; i += 1) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]] += 1;
+    }
+    if (hash[nums[i]] > 1) {
+      return true;
+    }
   }
-  if (hash[nums[i]] > 1) {
-    console.log(true);
-    break;
-  }
+  return false;
 }
-console.log(hash);
+
+console.log(containsDuplicate(nums)); // true

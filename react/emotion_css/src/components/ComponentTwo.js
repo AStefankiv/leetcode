d2fa6c3fcc36f3f1a468249bb3bae20f3ea3e5bb
AStefankiv <stefankif35@gmail.com>
2024-12-30 22:45:17 -0700

    Adjust height

--- react/emotion_css/src/components/ComponentTwo.js
+++ react/emotion_css/src/components/ComponentTwo.js
@@ -3,8 +3,26 @@ import { css } from '@emotion/react';
 
 const ComponentTwo = () => {
   return (
-    <div>
-      Component Two
+    <div css={css`
+      display: flex;
+      flex-direction: row;
+      gap: 20px;
+      align-items: flex-start;
+      `}>
+      <div
+      css={css`
+        font-size: 24px;
+        font-weight: bold;
+        width: 50%;
+        `}
+      >About Us</div>
+      <div
+      css={css`
+        font-size: 20px;
+        width: 130%;
+        `}
+      >Nano Folio is simple HTML template with a minimal very light-weight custom CSS file. Max content width is 940 pixels. You can easily change the width or any part in this template. Gallery comes with a pop-up image and simple categories.
+Please support Tooplate website by telling your friends about it. That is going to be very helpful for us. Feel free to message us via email or live chat if you have anything to say.</div>
     </div>
   )
 }

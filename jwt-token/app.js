9a621ad724ad6adae6b7dd48429cc0448d541edf
AStefankiv <stefankif35@gmail.com>
2025-01-02 22:35:33 -0700

    Create mini auth + login

--- jwt-token/app.js
+++ jwt-token/app.js
@@ -20,3 +20,26 @@ app.post('/register', (req, res) => {
   res.status(201).json({ message: 'User registered successfully' });
 });
 
+// User login endpoint
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid username or password' });
+  }
+  const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+  res.status(200).json({ token });
+});
+
+// Middleware to verify JWT
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+

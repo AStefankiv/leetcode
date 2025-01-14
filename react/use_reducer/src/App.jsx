1128c0fab6e83de05a335f7379ff74426810f51f
AStefankiv <stefankif35@gmail.com>
2025-01-13 22:37:42 -0700

    Create Todos

--- react/use_reducer/src/App.jsx
+++ react/use_reducer/src/App.jsx
@@ -1,34 +1,43 @@
 import { useReducer } from "react"
-import './App.css'
+
+// const reducer = (state, action) => {
+//   if (action.type === 'increment') {
+//     return { count: state.count + 1 }
+//   }
+//   if (action.type === 'decrement') {
+//     return { count: state.count - 1 }
+//   }
+//   throw new Error("Unknown action type")
+// }
+
+// const Counter = () => {
+//   const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+//   return (
+//     <>
+//       <h1>{state.count}</h1>
+//       <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+//       <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+//     </>
+//   )
+// }
+
+// export default Counter;
+
+
 
 const reducer = (state, action) => {
-  // switch (action.type) {
-  //   case 'increment':
-  //     return { count: state.count + 1 }
-  //   case 'decrement':
-  //     return { count: state.count - 1 }
-  //   default:
-  //     throw new Error("Unknown action type")
-  // }
-  if (action.type === 'increment') {
-    return { count: state.count + 1 }
+  if (action.type = 'add') {
+    return [...state, { id: Date.now(), text: action.payload, completed: false }]
   }
-  if (action.type === 'decrement') {
-    return { count: state.count - 1 }
+  if (action.type = 'toggle') {
+    return state.map((todo) => todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo)
+  };
+  if (action.type = 'delete') {
+    return state.filter((todo) => todo.id !== action.payload)
   }
   throw new Error("Unknown action type")
 }
 
-const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
-
-  return (
-    <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
-    </>
-  )
-}
-
-export default Counter;
+const Todo = () => {
+  const []

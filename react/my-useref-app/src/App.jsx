c4ea87a5d6dad162a933b519780f7617bbd0c28d
AStefankiv <stefankif35@gmail.com>
2025-01-19 17:43:45 -0700

    useCallback - 1

--- react/my-useref-app/src/App.jsx
+++ react/my-useref-app/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState, useMemo } from 'react'
+import { useState, useCallback} from 'react'
 import './App.css'
 
 function App() {
-  const [number, setNumber] = useState(1);
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
 
-  // Expensive calculation
-  const factorial = useMemo(() => {
-    console.log("Calculating factorial...");
-    const computeFactorial = (n) => (n <= 1 ? 1 : n * computeFactorial(n - 1));
-    return computeFactorial(number);
-  }, [number]);
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1)
+  }, [count]);
 
   return (
-    <div>
-      <h1>Factorial of {number}: {factorial}</h1>
-      <button onClick={() => setNumber(number + 1)}>Increase Number</button>
-      <button onClick={() => setNumber(number - 1)}>Decrease Number</button>
-
-      {/* <h2>Counter: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increase Counter</button> */}
+    <div className="App">
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <input
+      type="text"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder='Type something...'
+      />
     </div>
-  );
+  )
 }
 
 export default App;

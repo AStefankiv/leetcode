26698f997d21f4c718b5a933676155ff79de7fdb
AStefankiv <stefankif35@gmail.com>
2025-06-11 20:21:10 -0600

    Commit

--- 1.js
+++ 1.js
@@ -1,13 +1,6 @@
-const nums = [2, 7, 11, 15], target = 18;
+const num = 121;
+const numStr = num.toString();
+const reverseNum = numStr.split('').reverse().join('');
+const isPalindrome = numStr === reverseNum;
 
-const twoSum = (nums, target) => {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-    return [];
-}
-console.log(twoSum(nums, target));
+console.log(isPalindrome ? `${num} is palindrome` : `${num} isn't palindrome`);

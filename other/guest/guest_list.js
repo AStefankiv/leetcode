083eba7fd2ee7e3b02bb21d48aba0dcf8f213d72
AStefankiv <stefankif35@gmail.com>
2025-06-30 22:47:15 -0600

    Homework

---
+++ other/guest/guest_list.js
@@ -0,0 +1,9 @@
+// const guestListEl = document.getElementById("guestList");
+// const addGuestForm = document.getElementById("addGuestForm");
+// const guestNameInput = document.getElementById("guestName");
+
+// addGuestForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const name = guestNameInput.ariaValueMax.trim();
+//   if (!name) return;
+// })

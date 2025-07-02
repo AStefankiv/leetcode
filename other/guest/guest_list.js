5e62f74f06d8213f33fe6c00aec88cbf912f677e
AStefankiv <stefankif35@gmail.com>
2025-07-01 19:41:47 -0600

    Homework

--- other/guest/guest_list.js
+++ other/guest/guest_list.js
@@ -1,9 +1,16 @@
-// const guestListEl = document.getElementById("guestList");
-// const addGuestForm = document.getElementById("addGuestForm");
-// const guestNameInput = document.getElementById("guestName");
+const guestListEl = document.getElementById("guestList");
+const addGuestForm = document.getElementById("addGuestForm");
+const guestNameInput = document.getElementById("guestName");
 
-// addGuestForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const name = guestNameInput.ariaValueMax.trim();
-//   if (!name) return;
-// })
+addGuestForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = guestNameInput.value.trim();
+  if (!name) return;
+
+  const li = document.createElement("li");
+  li.textContent = name;
+  guestListEl.appendChild(li);
+
+  guestNameInput.value = "";
+  guestNameInput.focus();
+})
